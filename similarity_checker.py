f34611a2611ae35c832ba73469a32eac6b653b5d
LHH778 <1150939653@qq.com>
2024-09-11 23:25:57 +0800

    增加了对于异常情况hamming_dist == -1的处理

--- similarity_checker.py
+++ similarity_checker.py
@@ -13,9 +13,28 @@ class SimilarityChecker:
         # 计算汉明距离
         hamming_dist = SimHashService.hamming_distance(simhash1, simhash2)
 
+        # 对汉明距离为 -1 的情况进行异常处理
+        if hamming_dist == -1:
+            # raise InvalidHammingDistanceError("无效的汉明距离，可能是SimHash长度不一致或输入无效")
+            print("无效的汉明距离，可能是SimHash长度不一致或输入无效")
+            return -1
+
         print(f"汉明距离: {hamming_dist}")
 
         # 计算相似率
         similarity_rate = (64 - hamming_dist) / 64 * 100
 
         return similarity_rate
+
+    # def check_similarity_with_exception_handling(self, text1, text2):
+    #     try:
+    #         return self.check_similarity(text1, text2)
+    #     except InvalidHammingDistanceError as e:
+    #         print(f"异常捕获: {str(e)}，相似率设置为1")
+    #         return -1  # 返回相似率为 -1
+
+
+# class InvalidHammingDistanceError(Exception):
+#     """自定义异常类，用于处理无效的汉明距离"""
+#
+#     pass
