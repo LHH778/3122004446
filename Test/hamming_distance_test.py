0806c0118c2e63483fd638aa164ced757af3408b
LHH778 <1150939653@qq.com>
2024-09-11 22:32:34 +0800

    hamming_distance的测试

---
+++ Test/hamming_distance_test.py
@@ -0,0 +1,42 @@
+import unittest
+from ..simhash_service import SimHashService
+from ..similarity_checker import SimilarityChecker
+from ..data_preprocessing import DataPreprocessing
+
+class TestHammingDistance(unittest.TestCase):
+
+    def test_both_none(self):
+        # 测试两个输入都为 None
+        result = SimHashService.hamming_distance(None, None)
+        self.assertEqual(result, -1)
+
+    def test_one_none(self):
+        # 测试其中一个输入为 None
+        result = SimHashService.hamming_distance("abc", None)
+        self.assertEqual(result, -1)
+        result = SimHashService.hamming_distance(None, "abc")
+        self.assertEqual(result, -1)
+
+    def test_different_lengths(self):
+        # 测试输入长度不同
+        result = SimHashService.hamming_distance("abc", "abcd")
+        self.assertEqual(result, -1)
+
+    def test_same_strings(self):
+        # 测试两个完全相同的字符串，汉明距离应该为 0
+        result = SimHashService.hamming_distance("abc", "abc")
+        self.assertEqual(result, 0)
+
+    def test_partial_different(self):
+        # 测试部分字符不同的字符串
+        result = SimHashService.hamming_distance("abc", "abd")
+        self.assertEqual(result, 1)
+
+    def test_completely_different(self):
+        # 测试所有字符都不同的字符串
+        result = SimHashService.hamming_distance("abc", "xyz")
+        self.assertEqual(result, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
